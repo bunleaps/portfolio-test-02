4e022859aefae578772aff63a34664cb584a5952
bunleaps <bunleap.sorn@gmail.com>
2025-07-04 11:27:26 +0700

    build fix

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div className="p-8 w-full">
       <div className="w-1/2 mx-auto">
         <p className="text-center">
-          Hi, I'm <span className="text-blue-600 font-semibold">Buns</span>. I
-          turn iced coffee into websites and bugs into features.
+          Hi, I&#39;m <span className="text-blue-600 font-semibold">Buns</span>.
+          I turn iced coffee into websites and bugs into features.
         </p>
         <h1 className="text-3xl font-bold text-center">
           Keep it clean, keep it clever, and ship it.
